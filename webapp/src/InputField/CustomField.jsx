417e07aa8b8166c8564c1932326ed7fff5704673
naveen2707TN <naveenvishnu1007@gmail.com>
2026-04-29 20:23:35 +0530

    removed unused items

--- webapp/src/InputField/CustomField.jsx
+++ webapp/src/InputField/CustomField.jsx
@@ -7,7 +7,7 @@ const CustomField = ({ type, name, placeholder, pass, errmsg, onchange, value, l
       <div className={classes.ip_fields}>
         <img alt='x-ico' src={img} />
         <input type={type} name={name} placeholder={placeholder} onChange={onchange} value={value} className={classes.inputs} required autoComplete='off' onKeyDown={(e) => onKeys(e, ind)} ref={(el) => ref.current[ind] = el} />
-        <label className={value && value.length > 0 ? classes.labels_Selected : classes.labels} style={errmsg == "" ? { top: "-10px" } : { top: "-9px" }}>{label}</label>
+        <label className={value && value.length > 0 ? classes.labels_Selected : classes.labels} style={errmsg === "" ? { top: "-10px" } : { top: "-9px" }}>{label}</label>
         
       </div>
       <label className={classes.err}>{errmsg}</label>
